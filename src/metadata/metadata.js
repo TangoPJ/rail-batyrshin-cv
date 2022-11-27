9b329b62689b393b81e71846af40808a81e183bc
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-11-27 14:55:28 +0600

    Updated information

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,52 +1,53 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Rail Batyrshin',
+  title: 'Frontend Engineer',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
+    'Residence': '<a href="https://goo.gl/maps/3b2fDvoMJB2c65nW9"><i class="fa fa-home fact-icon"></i>Kyrgyzstan, Osh</a>',
+    'Linkedln': `<a href="https://www.linkedin.com/in/rail-batyrshin-852125235/">
+                  <i class="fa-brands fa-linkedin fact-icon"></i>Rail Batyrshin
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>Gmail</a>',
+    'Mobile': '<a href="tel:96555240064"><i class="fa fa-phone fact-icon"></i>+996555240064</a>',
+    'GitHub': '<a href="https://github.com/TangoPJ"><i class="fa-brands fa-github fact-icon"></i>TangoPJ</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['HTML'],
+    ['CSS'],
+    ['JavaScript'],
+    ['TypeScript'],
+    ['VueJS'],
+    ['NuxtJS'],
+    ['Quasar'],
+    ['React/Redux'],
+    ['NextJS'],
+    ['SPA'],
+    ['API'],
+    ['Prismic CMS'],
+    ['Node.js'],
+    ['Express.js'],
+    ['NestJS'],
+    ['Postgresql'],
+    ['Jira'],
+    ['Git'],
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      company: 'MadDevs',
+      location: 'Bishkek, Kyrgyzstan',
+      title: 'Frontend developer',
+      period: 'May 2021 - Present',
+      skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue3.js/Quasar', 'UMA-connector', 'Git', 'Jira'],
       contents: `
 Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
-    },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      company: 'MadDevs',
+      location: 'Bishkek, Kyrgyzstan',
+      title: 'Frontend developer',
+      period: 'Sep 2021 - Aug 2022',
+      skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue2.js/NuxtJS', 'Prismic CMS', 'React/Redux', 'NestJS', 'Postgreql', 'Git', 'Jira'],
+      contents: `I with marketing team developed the website (https://maddevs.io) - in this process we used Vue2.js and Prismic CMS on the frontend and Express.js on the backend. I participated in the Radiator project, which is designed to collect metrics from Google Analytics, Glassdoor, Quora and to send data to Slack channel as a report.`
     }
   ],
   experience: [
