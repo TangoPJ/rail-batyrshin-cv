2082e2ed71e8582c0e55ae105f8099429d155e48
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-11-29 13:08:46 +0600

    Updated information

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -65,18 +65,13 @@ module.exports = {
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      header: 'Web development',
+      skills: ['HTML', 'CSS', 'NextJS', 'Express', 'Prismic'],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      * In my spare time I develop my website using modern technologies railbatyrshin.me to improve my hard skills. To create articles I'll use the Prismic CMS.
+      * Some of my interests and hobbies revolve around GitHub and open source community.
+      A complete list of side/hobby projects: https://github.com/TangoPJ
+      `
     },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
   ]
 };
