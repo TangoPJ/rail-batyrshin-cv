c0420eff2b81f05f16e3678951dae3f76365bcdd
Rail Batyrshin <rail.batyrshin.js@gmail.com>
2022-11-29 21:25:57 +0600

    Updated information

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Rail Batyrshin',
   title: 'Frontend Engineer',
   facts: {
-    'Residence': '<a href="https://goo.gl/maps/3b2fDvoMJB2c65nW9"><i class="fa fa-home fact-icon"></i>Kyrgyzstan, Osh</a>',
+    'Residence': '<a href="https://goo.gl/maps/3b2fDvoMJB2c65nW9"><i class="fa fa-home fact-icon"></i>Osh, Kyrgyzstan</a>',
     'Linkedln': `<a href="https://www.linkedin.com/in/rail-batyrshin-852125235/">
                   <i class="fa-brands fa-linkedin fact-icon"></i>Rail Batyrshin
                 </a>`,
@@ -21,7 +21,6 @@ module.exports = {
     ['React/Redux'],
     ['NextJS'],
     ['SPA'],
-    ['API'],
     ['Prismic CMS'],
     ['Node.js'],
     ['Express.js'],
@@ -36,40 +35,44 @@ module.exports = {
       location: 'Bishkek, Kyrgyzstan',
       title: 'Frontend developer',
       period: 'May 2021 - Present',
-      skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue3.js/Quasar', 'UMA-connector', 'Git', 'Jira'],
+      skills: ['HTML', 'SCSS', 'TypeScript', 'Vue3.js/Quasar'],
       contents: `
       Frontend development and production environment support for enji.ai
-        * Migrated a web application from vue 2 to vue 3
-        * Development and support of the personal panel
-        * Increased the creation of forms on the frontend by ~3 times
-        * Developed a custom table for financial reports`
+        - Migrated a web application from vue 2 to vue 3
+        - Development and support of the personal panel
+        - Increased the creation of forms on the frontend by ~3 times
+        - Developed a custom table for financial reports`
     },
     {
       company: 'MadDevs',
       location: 'Bishkek, Kyrgyzstan',
       title: 'Frontend developer',
       period: 'Sep 2021 - Aug 2022',
-      skills: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Vue2.js/NuxtJS', 'Prismic CMS', 'React/Redux', 'NestJS', 'Postgreql', 'Git', 'Jira'],
+      skills: ['HTML', 'SCSS', 'TypeScript', 'Vue2.js/NuxtJS', 'Prismic CMS', 'React/Redux', 'NestJS', 'Postgreql'],
       contents: `
       Frontend and backend development and support for website maddevs.io:
-        * Created website using Vue2.js/Nuxt.js
-        * Created slices for build pages via Prismic CMS
-        * Wrote unit tests to the logic and components using jest engine, vue-test-library, vue-test-utils
+        - Supported for website development using Vue2.js/Nuxt.js
+        - Created slices for build pages via Prismic CMS
+        - Wrote unit tests to the logic and components using jest engine,
+          vue-test-library, vue-test-utils
 
-      Frontend and backend development and support for radiator app (use to get metrics from Google Analytics, Quora, Glassdoor, etc. and send to Slack channel as a report):
-        * Created UI by using ReactJS and AntDesign library
-        * Supported the backend when adding new functionality
-        * Supported migrations when changing the database
+      Frontend and backend development and support for radiator app (use to get 
+        metrics from Google Analytics, Quora, Glassdoor, etc. 
+        and send to Slack channel as a report):
+        - Created UI by using ReactJS and AntDesign library
+        - Supported the backend when adding new functionality
+        - Supported migrations when changing the database
       `
     }
   ],
   experience: [
     {
       header: 'Web development',
-      skills: ['HTML', 'CSS', 'NextJS', 'Express', 'Prismic'],
+      skills: ['HTML', 'SCSS', 'TypeScript', 'NextJS', 'Express', 'Prismic CMS'],
       contents: `
-      * In my spare time I develop my website using modern technologies railbatyrshin.me to improve my hard skills. To create articles I'll use the Prismic CMS.
-      * Some of my interests and hobbies revolve around GitHub and open source community.
+      - In my spare time I develop my website http://railbatyrshin.me using modern technologies to improve my hard skills.
+      To create articles I'll use the Prismic CMS.
+      - Some of my interests and hobbies revolve around GitHub and open source community.
       A complete list of side/hobby projects: https://github.com/TangoPJ
       `
     },
